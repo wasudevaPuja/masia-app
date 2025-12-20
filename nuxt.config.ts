f1e8d9b79d76860f513476f5d237e8afa7787967
wasudewa puja <wasudewa223@gmail.com>
2025-12-20 22:57:37 +0800

    update sitemap url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
   site: {
-    url: 'https://masiaglobal.asia'
+    url: 'https://www.masiaglobal.asia'
   },
 
   routeRules: {
